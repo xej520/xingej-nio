2fce177487f1501a27c28a172885e24bd6c4f33c
故、新 <xingej@163.com>
2017-10-01 07:57:30 +0800

    1、主要是更新了ByteBufferTest.java注释

--- src/main/java/xingej/buffer/test001/ByteBufferTest.java
+++ src/main/java/xingej/buffer/test001/ByteBufferTest.java
@@ -10,24 +10,41 @@ public class ByteBufferTest {
         ByteBuffer buffer = ByteBuffer.allocate(8);
         //打印出初始状态下position, limit, capacity的值
         System.out.println("---->position:\t" + buffer.position() + "\n---->limit:\t" + buffer.limit() + "\n---->capacity:\t" + buffer.capacity());
-        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+
+        //-------------------------------测试-----put----操作---------------------------------------------------------------
+        System.out.println("-----put---操作---前----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
         //往缓冲区里，添加一个字符“h”,再查看position, limit, capacity的值
-        buffer.put("h2".getBytes());
-        System.out.println("---->position:\t" + buffer.position() + "\n---->limit:\t" + buffer.limit() + "\n---->capacity:\t" + buffer.capacity());
-        System.out.println("--------------------------------------------------------------------------------------------------------------------");
-        //重新设定缓存可以存储的容量大小是2个字节，
-        //很明显，如果实际存储的字节大小，超过2个字节的话，就会抛异常的
-        buffer.limit(2);
+        buffer.put("h".getBytes());
+        System.out.println("-----put---操作---后----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
+
+        //-------------------------------测试-----flip----操作---------------------------------------------------------------
+        System.out.println("\n-----flip---操作---前----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
+        buffer.flip();
+        System.out.println("-----flip---操作---后----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
+
+        //-------------------------------测试-----get----操作---------------------------------------------------------------
+        System.out.println("\n-----get---操作---前----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
+        System.out.println("-----从缓存里取数据------:\t" + buffer.get());
+        System.out.println("-----get---操作---后----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
+
+
+        //-------------------------------测试-----limit----操作---------------------------------------------------------------
+        //重新设定缓存可以存储的容量大小是1个字节，
+        //很明显，如果实际存储的字节大小，超过1个字节的话，就会抛异常的
+        System.out.println("\n-----limit---操作---前----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
+        buffer.limit(1);
         try{
-            //因为，缓存里已经存储里h2两个字节，因此,ell是不会存储到缓存里的，而且会抛异常的
+            //因为，缓存里已经存储里h两个字节，因此,ell是不会存储到缓存里的，而且会抛异常的
             buffer.put("ell".getBytes());
         } catch (Exception e) {
-
+//            e.printStackTrace();
         }
-        System.out.println("---->position:\t" + buffer.position() + "\n---->limit:\t" + buffer.limit() + "\n---->capacity:\t" + buffer.capacity());
+        System.out.println("-----limit---操作---后----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
 
-        buffer.flip();
-        System.out.println("----->:\t" + new String(buffer.array()));
+        //-------------------------------测试-----clear----操作---------------------------------------------------------------
+        System.out.println("\n-----clear---操作---前----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
+        buffer.clear();
+        System.out.println("-----clear---操作---后----------->position:\t" + buffer.position() + "  ---->limit:\t" + buffer.limit() + "  ---->capacity:\t" + buffer.capacity());
     }
 
 }
--- src/main/java/xingej/channel/test002/BufferTest.java
+++ src/main/java/xingej/channel/test002/BufferTest.java
@@ -1,4 +1,13 @@
 package xingej.channel.test002;
 
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
 public class BufferTest {
+
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(8);
+
+    }
 }
